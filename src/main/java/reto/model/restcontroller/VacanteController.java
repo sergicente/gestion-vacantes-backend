c953916e5b1dad1cd2b6178f8fb9fcbaaea9464b
sergicente <sergicente@icloud.com>
2025-03-21 17:57:59 +0100

    Añadido CrossOrigin en vacante controller

--- src/main/java/reto/model/restcontroller/VacanteController.java
+++ src/main/java/reto/model/restcontroller/VacanteController.java
@@ -3,6 +3,7 @@ package reto.model.restcontroller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import reto.model.service.VacanteService;
 
 @RestController
 @RequestMapping("/api/vacantes")
+@CrossOrigin(origins = "*")
 public class VacanteController {
 	
 	@Autowired
