e617a94d9844063647b5b69470c77ffaec3ac95c
Cortes Villena Martínez <cortes1992@gmail.com>
2025-03-30 22:41:53 +0100

    Vacante POST, metodo actualizado para permitir recidbir ids y no objectos

---
+++ src/main/java/reto/model/dto/VacanteInputDto.java
@@ -0,0 +1,28 @@
+package reto.model.dto;
+
+import java.io.Serializable;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VacanteInputDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int idVacante;
+	private String nombre;
+	private String descripcion;
+	private LocalDate fecha;
+	private double salario;
+	private boolean destacado;
+	private String imagen;
+	private String detalles;
+	private int idEmpresa;
+	private int idCategoria;
+}
