e8d9875b8ec9d43ebd1aa2b13dee85c5e7d3051d
Sergi Centellas <sergicente@icloud.com>
2025-04-30 12:12:39 +0200

    Implementado sistema de búsqueda de vacantes

--- src/main/java/reto/model/repository/VacanteRepository.java
+++ src/main/java/reto/model/repository/VacanteRepository.java
@@ -19,4 +19,5 @@ public interface VacanteRepository extends JpaRepository<Vacante, Integer>{
     
     @Query("SELECT s FROM Solicitud s WHERE s.vacante.id = :idVacante")
     List<Solicitud> findSolicitudesByVacanteId(@Param("idVacante") Integer idVacante);
+    List<Vacante> findByNombreContainingIgnoreCaseOrDescripcionContainingIgnoreCase(String nombre, String descripcion);
 }
